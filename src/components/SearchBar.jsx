314728bfcff5c8d3fae255439adcb45bc0aa4c25
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-04-10 10:21:57 +0300

    search bar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+export function SearchBar() {
+	return (
+		<div className="bg-white rounded-md h-10 w-full flex justify-between px-4">
+			<input
+				type="text"
+				placeholder="Search blogs"
+				className="outline-0"
+			/>
+			<select name="" id="">
+				<option value="All" selected>
+					All Posts
+				</option>
+				<option value="Tech">Tech</option>
+			</select>
+		</div>
+	);
+}
