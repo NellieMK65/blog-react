9b2a0e8816da76232609d3e0d73074cabf773b52
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-04-11 10:55:04 +0300

    state, event handling, form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BlogCard } from './components/BlogCard';
 import { FeaturedBlogCard } from './components/FeaturedBlogCard';
 import { SearchBar } from './components/SearchBar';
@@ -19,7 +20,7 @@ const blogTwo = {
 // array iterating methods
 // map, find, reduce, forEach, filter
 // .map -> iterate over the array and return the modified array
-const blogs = [
+const data = [
 	{
 		id: 1,
 		title: 'React',
@@ -49,11 +50,46 @@ const blogs = [
 
 // the spread operator creates a copy at this point
 // const obj = { ...blogOne };
+const { image, title } = blogOne;
+const numbers = [1, 2, 3];
+const [one, two, four] = numbers;
+
+const Button = () => {
+	return <button className="p-3 bg-blue-300">My Button</button>;
+};
 
 function App() {
+	// React state -> dynamic data in a component
+	const [count, updateCount] = useState(0);
+	const [blogs, setBlogs] = useState(data);
+
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [category, setCategory] = useState('');
+	const [image, setImage] = useState('');
+
+	// TO BE COVERED LATER
+	// useEffect(() => {
+	// 	fetch('http://localhost:3000/blogs')
+	// 		.then((res) => res.json())
+	// 		.then((data) => {
+	// 			setBlogs(data);
+	// 		});
+	// }, []);
+
 	return (
 		<main className="min-h-screen flex p-8 gap-3 bg-gray-100 font-poppins">
 			<div className="w-[70%] space-y-3">
+				<button
+					className="border bg-amber-300 p-3"
+					onClick={() => {
+						console.log('Button clicked');
+						updateCount(count + 1);
+					}}
+				>
+					Count {count}
+				</button>
+
 				<h1 className="font-semibold text-2xl">Latest Blogs</h1>
 				<SearchBar />
 
@@ -82,8 +118,8 @@ function App() {
 					<BlogCard /> */}
 
 					{/* List and keys */}
-					{blogs.map((blog) => (
-						<BlogCard key={blog.id} {...blog} />
+					{blogs.map((blog, index) => (
+						<BlogCard key={index} {...blog} />
 					))}
 
 					{/* {blogs.map(function (blog) {
@@ -91,7 +127,74 @@ function App() {
 					})} */}
 				</div>
 			</div>
-			<div className="flex-1">News</div>
+			<div className="flex-1">
+				News
+				<form
+					action=""
+					className="border p-2 rounded-xl w-full space-y-3"
+					onSubmit={(e) => {
+						e.preventDefault();
+						console.log({ title, description, category, image });
+						// update blogs with new values from form
+						setBlogs([
+							...blogs,
+							...[{ title, description, category, image }],
+						]);
+						// clear form
+						setTitle('');
+					}}
+				>
+					<div>
+						<label htmlFor="">Title</label>
+						<input
+							type="text"
+							placeholder="title"
+							className="p-2 bg-white border w-full rounded-lg"
+							onChange={(e) => {
+								setTitle(e.target.value);
+							}}
+						/>
+					</div>
+					<div>
+						<label htmlFor="">Description</label>
+						<textarea
+							name=""
+							id=""
+							placeholder="description"
+							className="p-2 bg-white border w-full rounded-lg"
+							onChange={(e) => {
+								setDescription(e.target.value);
+							}}
+						></textarea>
+					</div>
+					<div>
+						<label htmlFor="">Category</label>
+						<input
+							type="text"
+							placeholder="Category"
+							className="p-2 bg-white border w-full rounded-lg"
+							onChange={(e) => {
+								setCategory(e.target.value);
+							}}
+						/>
+					</div>
+					<div>
+						<label htmlFor="image">Image</label>
+						<input
+							id="image"
+							type="text"
+							placeholder="image"
+							className="p-2 bg-white border w-full rounded-lg"
+							onChange={(e) => {
+								setImage(e.target.value);
+							}}
+						/>
+					</div>
+					<button className="w-full p-2 bg-amber-300 rounded-lg">
+						Save
+					</button>
+				</form>
+			</div>
 		</main>
 	);
 }
